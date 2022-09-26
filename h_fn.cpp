8d63c8f38dfe6c540ce2607f97efa4b4bc4ed3e4
Emma Govan <67744546+emmagovan@users.noreply.github.com>
2022-09-26 11:09:02 +0100

    Update h_fn.cpp

--- h_fn.cpp
+++ h_fn.cpp
@@ -33,11 +33,11 @@ NumericVector hfn(NumericVector theta, int n_sources){
 
 //[[Rcpp::export]]
 List hcpp(NumericVector p, int n_sources, int n_isotopes,
-         NumericMatrix concentrationmeans, NumericMatrix sourcemeans,
-         NumericMatrix correctionmeans,
-         NumericMatrix corrsds, NumericMatrix sourcesds, NumericMatrix theta, NumericMatrix y ){
+          NumericMatrix concentrationmeans, NumericMatrix sourcemeans,
+          NumericMatrix correctionmeans,
+          NumericMatrix corrsds, NumericMatrix sourcesds, NumericMatrix theta, NumericMatrix y ){
   double x =0;
-
+  
   
   double ly = y.rows();
   
@@ -52,16 +52,16 @@ List hcpp(NumericVector p, int n_sources, int n_isotopes,
     // mean was zero - this caused problems below when I needed to take the log of the mean
     prior_means(i) = 1;
     prior_sd(i) = 1;
-    }
+  }
   
   for (int i = 0; i<n_isotopes; i++){
     c_0(i) = 1;
     d_0(i) = 1;
-    }
-    
+  }
+  
   
   if(n_isotopes == 2){
-
+    
     
     // This is to get dnorm(y[,1], sum(p*q) etc) 
     double mutop1 = 0;
@@ -87,7 +87,7 @@ List hcpp(NumericVector p, int n_sources, int n_isotopes,
     //Calculate mu and sd
     mu1 = mutop1/mubtm1;
     sigmasq1 = sigmatopsq1/sigmabtmsq1;
-    double sigma1 = pow(sigmasq1, 0.5) + 1/theta(0, (n_isotopes));
+    double sigma1 = pow(sigmasq1 + 1/theta(0, (n_sources)), 0.5);
     
     
     // This is to get dnorm(y[,2], sum(p*q) etc) 
@@ -111,26 +111,30 @@ List hcpp(NumericVector p, int n_sources, int n_isotopes,
     mu2 = mutop2/mubtm2;
     sigmasq2 = sigmatopsq2/sigmabtmsq2;
     
-    double sigma2 = pow(sigmasq2, 0.5) + theta(0, 1+n_isotopes);
+    double sigma2 = pow(sigmasq2 + 1/theta(0, (1+n_sources)), 0.5);
     
     double yminusmu1 = 0;
     double yminusmu2 = 0;
     
     for(int i = 0; i<ly; i++){
-      yminusmu1 += y(i,0) - mu1;
-      yminusmu2 += y(i,1) - mu2;
+      yminusmu1 += pow((y(i,0) - mu1),2);
+      yminusmu2 +=  pow((y(i,1) - mu2),2);
     }
     
     
     
     // This is log(dnorm(y, p*q, p^2*q^2 etc) for y1 and y2
     
-    x = - ly * log(sigma1) - 0.5 * ly * log(pow(M_PI,2)) 
-        - 0.5 * (pow((yminusmu1),2)) * 1/(pow(sigma1,2)) 
-        - ly * log(sigma2) - 0.5 * ly * log(pow(M_PI,2)) 
-        - 0.5 * (pow((yminusmu2),2)) * 1/(pow(sigma2,2));
+    x = - ly * log(sigma1) - 0.5 * ly * log(2 * M_PI)
+      - 0.5 * yminusmu1 * 1/(pow(sigma1,2))
+      - ly * log(sigma2) - 0.5 * ly * log(2 * M_PI)
+      - 0.5 * yminusmu2 * 1/(pow(sigma2,2));
+     
     
 
+ 
+ 
+      
       
       
   }  else{
@@ -138,15 +142,15 @@ List hcpp(NumericVector p, int n_sources, int n_isotopes,
     
     
     
-   
+    
     
     // This is to get dnorm(y[,1], sum(p*q) etc)
     double mutop = 0;
     double mubtm = 0;
     double mu = 0;
-    double sigma = 0;
-    double sigmatop = 0;
-    double sigmabtm = 0;
+    double sigmasq = 0;
+    double sigmatopsq = 0;
+    double sigmabtmsq = 0;
     
     // calculate mu numerator and denominator
     for(int i=0; i<n_sources; i++){
@@ -154,54 +158,70 @@ List hcpp(NumericVector p, int n_sources, int n_isotopes,
       mubtm += p(i) * concentrationmeans(i,0);
     }
     
+
+    
+    
     // same for sigma
     for(int i=0; i<n_sources; i++){
-      sigmatop += pow(p(i),2) * pow(concentrationmeans(i,0),2) * (pow(sourcesds(i,0),2) + 
+      sigmatopsq += pow(p(i),2) * pow(concentrationmeans(i,0),2) * (pow(sourcesds(i,0),2) + 
         pow(corrsds(i,0),2));
-      sigmabtm += pow(p(i),2) * pow(concentrationmeans(i,0),2);
+      sigmabtmsq += pow(p(i),2) * pow(concentrationmeans(i,0),2);
     }
+    
+
     
     //Calculate mu and sd
     mu = mutop/mubtm;
-    sigma = sigmatop/sigmabtm;
-    double sigma_sq = pow(sigma,2);
+    sigmasq = sigmatopsq/sigmabtmsq;
+    double sigma = pow(sigmasq + 1/theta(0, n_sources), 0.5);
+    
+    
+    
+    
     
     double yminusmu = 0;
     
     for(int i = 0; i<ly; i++){
-      yminusmu += y(i,0) - mu;
-      }
+      yminusmu += pow((y(i,0) - mu),2);
+    }
     
-    
+
     
     
     
     // This has y
     
-    x = - ly * log(sigma) - 0.5 * ly * log(pow(M_PI,2)) - (pow((yminusmu),2))* 1/(2*sigma_sq);
+    x = - ly * log(sigma) - 0.5 * ly * log(2 * M_PI) - 0.5 * yminusmu* 1/(pow(sigma,2));
+    
+    
+
     
   }
   
   double thetanorm = 0;
-
+  
+  
+  
+  
   for(int i = 0; i<n_sources; i++){
-    thetanorm +=  - n_sources * log(prior_means(i)) - 0.5 * log(pow(M_PI,2)) - (pow((theta(0,i) - prior_means(i)), 2)
-    * 1/(2 * pow(prior_sd(i), 2)));
-    }
-
+    thetanorm +=  - n_sources * log(prior_means(i)) - 0.5 * log(2 * M_PI) - (pow((theta(0,i) - prior_means(i)), 2)
+                                                                                  * 1/(2 * pow(prior_sd(i), 2)));
+  }
+  
   double gammaprior = 0;
   for (int i=0; i <(n_isotopes); i++){
     gammaprior += c_0(i) * log(d_0(i)) - log(tgamma(c_0(i))) +(c_0(i) - 1) * theta(0,(i+n_sources)) - 
       d_0(i) * theta(0,(i+n_sources));
-
-    }
-
-  x = x + gammaprior + thetanorm;
-
-  return Rcpp::List::create(Rcpp::Named("total") = x,
+    
+  }
+  
+  double totx = x + gammaprior + thetanorm;
+  
+  return Rcpp::List::create(Rcpp::Named("total") = totx,
                             Rcpp::Named("gamma") = gammaprior,
-                            Rcpp::Named("theta") = thetanorm);
-
+                            Rcpp::Named("theta") = thetanorm,
+                            Rcpp::Named("ynorm") = x);
+  
   
 }
 
