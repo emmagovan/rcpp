7f95e32160057910cf111502ac528875962a0247
Emma Govan <67744546+emmagovan@users.noreply.github.com>
2022-09-30 14:35:56 +0100

    Add files via upload

--- log_q_fn.cpp
+++ log_q_fn.cpp
@@ -6,17 +6,14 @@ using namespace Rcpp;
 List log_q_cpp(NumericMatrix theta, NumericVector lambda, 
                         int n_sources, int n_tracers){
   
-  NumericVector mean(n_sources);
+
   
-  NumericVector thetavec = theta(1,_);
-  
-  for(int i = 0; i <n_sources; i++){
-    mean(i) = thetavec(i);
-    }
+  NumericVector thetavec = theta(0,_);
+
   
   NumericMatrix thetaminusmean(1, n_sources);
   for(int i = 0; i <n_sources; i++){
-    thetaminusmean(1,i) = thetavec(i) - mean(i);
+    thetaminusmean(0,i) = thetavec(i) - lambda(i);
     }
   
   NumericMatrix choldecomp(n_sources, n_sources);
@@ -41,16 +38,21 @@ List log_q_cpp(NumericMatrix theta, NumericVector lambda,
   
   for (int i = 0; i < thetaminusmean.nrow(); i++) 
   {
-    for (int j = 0; j < thetaminusmean.ncol(); j++) 
+    for (int j = 0; j < tcholdecomp.ncol(); j++) 
+      for (int k = 0; k < tcholdecomp.nrow(); k++){
     {
-      p1(i,j)=thetaminusmean(i,j) * tcholdecomp(i,j) ;
+      p1(i,j) =p1(i,j) + thetaminusmean(i,k) * tcholdecomp(k,j) ;
     }
   }
+  }
+  
 
-  double p1tp1;
-  for (int i = 0; i < p1.nrow(); i++) 
+
+
+  double p1tp1 = 0;
+  for (int i = 0; i < n_sources; i++) 
   {
-    p1tp1 += pow(p1(1,i),2);
+    p1tp1 += pow(p1(0,i),2);
   }
   
   
@@ -63,10 +65,32 @@ List log_q_cpp(NumericMatrix theta, NumericVector lambda,
     +(lambda(((n_sources + (n_sources * (n_sources + 1)) / 2) + i)) - 1) * theta(0,(i+n_sources)) - 
     lambda(((n_sources + (n_sources * (n_sources + 1)) / 2)) + n_tracers + i) * theta(0,(i+n_sources));
   }
+  
+  double sumlogdiag = 0;
+  for(int i = 0; i<choldecomp.nrow(); i++){
+    for(int j = 0; j<choldecomp.nrow(); j++){
+      if((i = j)){
+        sumlogdiag += log(choldecomp(i,j));
+        }
+      
+      }}
+  
  
- return -0.5 * n_sources * log(2 * M_PI) - 0.5 * sum(log(diag(choldecomp))) 
+ double x = -0.5 * n_sources * log(2 * M_PI) - 0.5 * sumlogdiag 
    - 0.5 * p1tp1 +gamman;
     
+    return Rcpp::List::create(Rcpp::Named("total") = x,
+                        Rcpp::Named("p1") = p1,
+                        Rcpp::Named("p1tp1") = p1tp1,
+                        Rcpp::Named("gammam") = gamman,
+                        Rcpp::Named("thetavec") = thetavec,
+                        Rcpp::Named("sumulogdiag") = sumlogdiag,
+                        Rcpp::Named("thminusmean") = thetaminusmean,
+                        Rcpp::Named("lambda") = lambda,
+                        Rcpp::Named("choldecomp") = choldecomp,
+                        Rcpp::Named("tcholdecomp") = tcholdecomp
+    )
+                        ;
  
 }
   
