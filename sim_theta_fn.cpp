5d2a4576ab948849de17a29b55ab9a1a67719ebf
Emma Govan <67744546+emmagovan@users.noreply.github.com>
2022-10-25 14:15:46 +0100

    Add files via upload

--- sim_theta_fn.cpp
+++ sim_theta_fn.cpp
@@ -1,5 +1,28 @@
-#include <Rcpp.h>
+#include <RcppArmadillo.h>
+#include <RcppDist.h>
 using namespace Rcpp;
+// [[Rcpp::depends(RcppArmadillo, RcppDist)]]
+// [[Rcpp::export]]
+
+arma::mat rMVNormC(const double n,
+                   const arma::vec mu,
+                   const arma::mat U) {
+  
+  // Dimension of MVN
+  int p = mu.size();
+  
+  // Simulate iid standard normals
+  arma::mat Z(p, n);
+  Z.imbue(norm_rand);
+  
+  // Now backsolve and add back on the means
+  arma::mat X = solve(U, Z);
+  for ( int i = 0; i < n; ++i ) {
+    X.col(i) += mu;
+  }
+  
+  return X.t();
+}
 
 
 
@@ -11,12 +34,51 @@ using namespace Rcpp;
 
 //[[Rcpp::export]]
 NumericMatrix sim_thetacpp(int S, NumericVector lambda, int n_sources, 
-                           int n_tracers, NumericMatrix rMVNorm){
+                           int n_tracers){
 NumericMatrix theta(S, (n_sources + n_tracers));
+  
+NumericVector mean(n_sources);
+
+for(int i = 0; i<n_sources; i++){
+  mean(i) = lambda(i);
+  }
+
+  
+  NumericMatrix chol_prec(n_sources, n_sources);
+  int count = 0;
+  for(int j = 0; j< n_sources; j++){ 
+    for(int i = 0; i<n_sources; i++){
+      if (i <= j){
+        count +=1;
+        chol_prec((i),(j)) = lambda(n_sources -1 +count);
+        
+        
+      }
+      else{
+        chol_prec(i,j) = 0;
+      }
+      
+    }
+  }
+  NumericMatrix tchol_prec = transpose(chol_prec);
+  
+  // 
+  // NumericMatrix rMVNorm(theta.nrow(), n_sources);
+  // 
+  // // now need to make rMVNormC(S, mu = mean, U = chol_preg)
+double p = (mean.length());
+  // NumericMatrix Z(p, S);
+  // for(int i = 0; i<(p*S); i++){
+  //   Z(_,i) = Rcpp::rnorm(p);
+  //   }
+  // 
+  // x = U^-1 Z
+  
+  
   
   for (int i = 0; i<n_sources; i++){
     
-      theta(_,i) = rMVNorm(_,i);
+      theta(_,i) = rMVNormC(S*p, mean, tcholprec);
     
 }
   NumericMatrix gammam(S, n_tracers);
@@ -34,3 +96,6 @@ NumericMatrix theta(S, (n_sources + n_tracers));
   return theta;
   
   }
+
+
+
